98b31b0050a3416f5931affff80c2a0fbe26be63
David James Carter <fresco.raja@gmail.com>
2015-07-08 13:00:32 -0700

    shorten description in map info view

--- app/assets/javascripts/views/map/info.js
+++ app/assets/javascripts/views/map/info.js
@@ -3,9 +3,19 @@ DigiSFere.Views.MapInfo = Backbone.View.extend({
 
   className: 'map-info-view',
 
+  initialize: function () {
+    this.shortInfo = this.model.get('about');
+    if (this.shortInfo.length > 130) {
+      var words = this.shortInfo.substr(0, 130).split(' ');
+      words.pop();
+      this.shortInfo = words.join(' ').concat('...');
+    }
+  },
+
   render: function () {
     var content = this.template({
-      listing: this.model
+      listing: this.model,
+      shortInfo: this.shortInfo
     });
     this.$el.html(content);
     return this;
